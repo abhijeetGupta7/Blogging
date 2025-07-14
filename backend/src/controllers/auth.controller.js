073f1679b7c33607f862296bcab5d15616c1dac4
abhijeetGupta7 <127014104+abhijeetGupta7@users.noreply.github.com>
2025-07-14 22:07:33 +0530

    fixes

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -64,6 +64,8 @@ const signin = async (req, res, next) => {
       .status(200)
       .cookie('access_token', token, {
         httpOnly: true,
+        secure: true, 
+        sameSite:"None"
       })
       .json(rest);
   } catch (error) {
